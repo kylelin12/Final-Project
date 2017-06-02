51deeb0c135d61ad01aaf3fac74b5a2eb4158a1c
Kyle <klin12@stuy.edu>
2017-06-02 13:50:21 -0400

    Added comments

--- Board.java
+++ Board.java
@@ -1,4 +1,3 @@
-import java.util.PriorityQueue;
 import java.util.LinkedList;
 
 // http://introcs.cs.princeton.edu/java/assignments/8puzzle.html
@@ -19,6 +18,21 @@ public class Board {
 	// set _previous to inputted int[][]
 	public void setPrevious(Integer[][] tiles) {
 		_previous = tiles;
+	}
+	
+	public Integer[][] getBoard(String option) {
+		if (option.equals("Previous") || option.equals("previous"))
+			return _previous;
+		else 
+			return _current;
+	}
+	
+	public int getMoves() {
+		return _movesDone;
+	}
+	
+	public void addMoves() {
+		_movesDone++;
 	}
 	
 	// return number of blocks out of place
@@ -48,10 +62,13 @@ public class Board {
 	
 	// does this board equal y
 	public boolean equals(Object y) {
+		// Basic check
 		if (y == this)
 			return true;
+		// If the object is null, not a Board or doesn't have the same dimensions as the board then false
 		if (y == null || !(y instanceof Board) || ((Board)y)._current.length != this._current.length)
 			return false;
+		// If the elements are mismatched
 		for (int i = 0; i < _current.length; i++)
 			for (int j = 0; j < _current.length; j++)
 				if (((Board)y)._current[i][j] != this._current[i][j])
@@ -69,13 +86,18 @@ public class Board {
 	public String toString() {
 		String string = "";
 		int index = 1;
-		for (Integer[] r: _current) {
-			for (Object c: r) {
-				
-				if (index % 3 == 0) {
-					string += c + "\n";
+		for (int i = 0; i < _current.length; i++) {
+			for (int j = 0; j < _current[i].length; j++) {
+				if (_current[i][j] == null) {
+					if (index % 3 == 0) // New line if the position is in the 3rd column
+						string += " \n";
+					else
+						string += " ";
 				} else {
-					string += c + " ";
+					if (index % 3 == 0) // New line if the position is in the 3rd column
+						string += _current[i][j] + "\n";
+					else 
+						string += _current[i][j] + " ";
 				}
 				index++;
 			}
@@ -88,8 +110,7 @@ public class Board {
 		Integer[][] three = {
 								{1, 2, 3},
 								{4, 5, 6},
-								{7, 8, },
-								{0, 5, 2}
+								{7, 8, null}
 							};
 		Board test = new Board(three);
 		System.out.println(test.toString());
--- Solver.java
+++ Solver.java
@@ -2,29 +2,39 @@ import java.util.PriorityQueue;
 
 public class Solver {
 
+	private Board _board;
+	private PriorityQueue _boardNeighbors;
+	
 	// find a solution to the initial board
 	public Solver(Board initial) {
-	
+		_board = initial;
 	}
 	
 	// is the initial board solvable?
 	public boolean isSolvable() {
-	
+		int size = 0;
+		for (int[] r: _board)
+			for (int c: r)
+				size++;
+		// If every index is filled
+		if (size == (_board.length * _board.length))
+			return false;
+		return true;
 	}
 	
 	// return min number of moves to solve the initial board
 	// -1 if no such solution
 	public int moves() {
-	
+		
 	}
 	
 	// return string representation of solution (as described above)
 	public String toString() {
-	
+		return _board.toString();
 	}
 	
 	//  read puzzle instance from stdin and print solution to stdout (in format above)
 	public static void main(String[] args) {
-	
+		
 	}
 }
